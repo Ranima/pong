246900a76d0576f1670368429e7e6b77095d28e5
Ranima <raphaelnmastel3@gmail.com>
2016-09-07 12:15:35 -0700

    closer to done

--- pongapp.h
+++ pongapp.h
@@ -31,6 +31,16 @@ void updatePaddle(Paddle &paddle) {
 	}
 }
 
+void updatePaddle2(Paddle &paddle) {
+	if (sfw::getKey(KEY_UP)) {
+		paddle.PositionY += paddle.Speed;
+	}
+
+	if (sfw::getKey(KEY_DOWN)) {
+		paddle.PositionY -= paddle.Speed;
+	}
+}
+
 //drawPaddle
 void drawPaddle(Paddle paddle) {
 	sfw::drawLine(paddle.PositionX, paddle.PositionY, paddle.PositionX, paddle.PositionY + paddle.Size);
@@ -41,10 +51,11 @@ struct Ball
 	float positionX, positionY;
 	float radius;
 	int steps;
-	float speed;
+	float speedX;
+	float speedY;
 };
 
-Ball spawnBall(float x, float y, float radius, int steps, float speed)
+Ball spawnBall(float x, float y, float radius, int steps, float speedX, float speedY)
 {
 	Ball placeholder;
 
@@ -52,26 +63,38 @@ Ball spawnBall(float x, float y, float radius, int steps, float speed)
 	placeholder.positionY = y;
 	placeholder.radius = radius;
 	placeholder.steps = steps;
-	placeholder.speed = speed;
+	placeholder.speedX = speedX;
+	placeholder.speedY = speedY;
 
 	return placeholder;
 }
 
-void updateBall(Ball &ball) 
+//#1 I need the ball to move...
+//#2 I need the ball to switch directions on the edges...
+//#3 I need the ball to switch directions on the paddle...
+void updateBall(Ball &ball, Paddle &player1, Paddle &player2) 
 {
-	ball.positionY += 1;
-	ball.positionX += 3;
-	if (ball.positionX == 900) {
-		ball.positionX -= 3;
+	ball.positionX += ball.speedX;
+	ball.positionY += ball.speedY;
+	if (ball.positionX > 900) {
+		ball.speedX *= -1;
+		ball.positionX = 900;
 	}
-	if (ball.positionX == 0) {
-		ball.positionX += 3;
+	if (ball.positionY > 500) {
+		ball.speedY *= -1;
+		ball.positionY = 500;
 	}
-	if (ball.positionY == 500) {
-		ball.positionY -= 1;
+	if (ball.positionX - ball.speedX < 0) {
+		ball.speedX *= -1;
+		ball.positionX = 0;
 	}
-	if (ball.positionY == 0) {
-		ball.positionY += 1;
+	if (ball.positionY - ball.speedY < 0) {
+		ball.speedY *= -1;
+		ball.positionY = 0;
+	}
+	if (ball.positionX == player1.PositionX && ball.positionY == player1.PositionY) {
+		ball.speedX *= -1;
+		ball.positionX = 41;
 	}
 }
 
@@ -81,7 +104,11 @@ void drawBall(Ball &ball) {
 //____________________________________________________________________________________________
 void buildCourt() {
 	sfw::initContext(900, 500);
+}
+
+void drawCourt() {
 	sfw::setBackgroundColor(BLACK);
+	sfw::drawLine(450, 2, 450, 498, RED);
 	sfw::drawLine(898, 498, 2, 498, GREEN);
 	sfw::drawLine(898, 2, 2, 2, GREEN);
 	sfw::drawLine(2, 2, 2, 498, BLUE);
